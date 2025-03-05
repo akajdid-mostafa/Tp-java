9f9d181a11d3792f8d9fc1a20dc0ec987075b15c
mostafa <mostafaakajdid6@gmail.com>
2025-03-05 11:44:47 +0000

    mos

--- src/main/java/Ecole2/App.java
+++ src/main/java/Ecole2/App.java
@@ -22,14 +22,11 @@ public class App {
         try {
             em.getTransaction().begin();
 
-            // Persister les étudiants avant de persister l'école
             em.persist(etudiant1);
             em.persist(etudiant2);
 
-            // Persister l'école
             em.persist(ecole1); // L'école et ses étudiants seront ajoutés à la base de données
 
-            // Validation de la transaction
             em.getTransaction().commit();
 
             System.out.println("L'école et les étudiants ont été ajoutés avec succès!");
