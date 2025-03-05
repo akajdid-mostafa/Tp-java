adda25672954409d020d91d4b5b73f999081b0e0
mostafa <mostafaakajdid6@gmail.com>
2025-03-05 12:20:56 +0000

    mos

---
+++ src/main/java/Etudient/partisaid.java
@@ -0,0 +1,71 @@
+package Etudient;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.SQLOutput;
+import javax.persistence.*;
+
+@Entity
+public class partisaid {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nom;
+    private String prenom;
+    private int age;
+
+
+    public partisaid() {
+    }
+
+    public partisaid(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    //Getters st Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "etudiant{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
